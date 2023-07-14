9316e3efa19a87309d9560086a6d06f2163758e3
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-15 01:34:14 +0530

    ququq

--- src/QueueUsingCollectionFramework.java
+++ src/QueueUsingCollectionFramework.java
@@ -2,9 +2,9 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 public class QueueUsingCollectionFramework {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         //Queue<Integer> q = new LinkedList();
-        Queue<Integer> q = new ArrayDeque();
+        Queue<Integer> q = new ArrayDeque<>();
         q.add(1);
         q.add(2);
         q.add(3);
